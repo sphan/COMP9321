b81cc655fba89a15802a51db94ce2e1aed9f63b8
sphan <santhai.phan@gmail.com>
2014-04-02 13:34:39 +1100

    Created a ControlServlet to navigate through JSPs.

---
+++ Assignment1/src/edu/unsw/comp9321/ControlServlet.java
@@ -0,0 +1,35 @@
+package edu.unsw.comp9321;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ControlServlet
+ */
+@WebServlet(urlPatterns="/control",displayName="ControlServlet")
+public class ControlServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ControlServlet() {
+		super();
+	}
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
