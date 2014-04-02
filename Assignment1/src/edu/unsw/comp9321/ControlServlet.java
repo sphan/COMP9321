9a1fb8a23954409abf5489611ab7a515fa2cdbb8
sphan <santhai.phan@gmail.com>
2014-04-02 17:47:48 +1100

    Wrote a search function to search for albums with matching substring.

--- Assignment1/src/edu/unsw/comp9321/ControlServlet.java
+++ Assignment1/src/edu/unsw/comp9321/ControlServlet.java
@@ -1,6 +1,7 @@
 package edu.unsw.comp9321;
 
 import java.io.IOException;
+import java.util.LinkedList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,6 +18,26 @@ public class ControlServlet extends HttpServlet {
 
 	public ControlServlet() {
 		super();
+		this.musicDb = new LinkedList<Album>();
+	}
+	
+	/**
+	 * Search in database for the album with matching substring.
+	 * @param searchString The substring in which the user typed in
+	 *     that is to be used in searching for the matching albums.
+	 * @return The list of albums with matching substring.
+	 */
+	public LinkedList<Album> searchForAlbum(String searchString) {
+		LinkedList<Album> results = new LinkedList<Album>();
+		
+		// Traverse the database and look for the album that contains
+		// the search string.
+		for (Album a : musicDb) {
+			if (a.getTitle().toLowerCase().contains(searchString.toLowerCase())) {
+				results.add(a);
+			}
+		}
+		return results;
 	}
 	
 	/**
@@ -29,7 +50,23 @@ public class ControlServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@SuppressWarnings("unchecked")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		// Always direct back to home page.
+		String nextPage = "welcome.jsp";
+		musicDb = (LinkedList<Album>) request.getAttribute("musicDb");
+		
+		if (action.equals("search")) {
+			String searchType = request.getParameter("searchType");
+			String searchString = request.getParameter("searchString");
+			if (searchType.equals("Album")) {
+				LinkedList<Album> results = searchForAlbum(searchString);
+			} else if (searchType.equals("Song")) {
+				
+			}
+		}
 	}
 
+	private LinkedList<Album> musicDb;
 }
