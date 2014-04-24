bfa795819c0628e7272571a0dba114aec710cdd6
Sandy <santhai.phan@gmail.com>
2014-04-24 13:22:01 +1000

    Changed content of web.xml

---
+++ Assignment 2/src/edu/unsw/comp9321/ControlServlet.java
@@ -0,0 +1,13 @@
+package edu.unsw.comp9321;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class ControlServlet
+ */
+@WebServlet(urlPatterns="/control",displayName="ControlServlet")
+public class ControlServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
