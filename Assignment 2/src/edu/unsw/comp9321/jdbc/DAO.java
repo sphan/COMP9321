3f7e82c64eba7ef2832749e0d8ed0f1eb687609d
mrJayson <mrJaayson@gmail.com>
2014-05-02 12:54:46 +1000

    filled out RoomDTO with private fields and getters/setters, filled out DAO's getAllRooms function, tested and correctly returns rooms

--- Assignment 2/src/edu/unsw/comp9321/jdbc/DAO.java
+++ Assignment 2/src/edu/unsw/comp9321/jdbc/DAO.java
@@ -16,59 +16,81 @@ public class DAO {
 	//This class should be the only class to talk to database
 	//Just keep adding more functions as we need them
 	//TODO must add insert functions here as well
-	
+
 	static Logger logger = Logger.getLogger(DAO.class.getName());
 	private Connection connection;
 	DateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
-	
+
 	public DAO () throws ServiceLocatorException, SQLException{
 		connection = DBConnectionFactory.getConnection();
 		logger.info("Got connection");
 	}
-	
+
 	//return all pending bookings
 	public List<BookingDTO> getAllPendingBookings() {
 		return null;
 	}
-	
+
 	//return all bookings including past completed ones
 	//unsure if we actually need this
 	public List<BookingDTO> getAllBookings() {
 		return null;
 	}
-	
+
 	//return a single booking from ID, plan to use for
 	//displaying individual booking information
 	public BookingDTO getBooking(String ID) {
 		return null;
 	}
-	
+
 	//return every single rooms, plan to use this for room occupancy
 	public List<RoomDTO> getAllRooms() {
-		return null;
+		List<RoomDTO> rooms = new ArrayList<RoomDTO>();
+		try {
+			Statement stmnt = connection.createStatement();
+			String query_cast = "SELECT * FROM ROOM";
+			ResultSet res = stmnt.executeQuery(query_cast);
+			logger.info("The result set size is "+res.getFetchSize());
+
+			while (res.next()) {
+				int id = res.getInt("id");
+
+				int room_number = res.getInt("room_number");
+				float price = res.getFloat("price");
+				float discounted_price = res.getFloat("discounted_price");
+				int room_type = res.getInt("room_type");
+				int availability = res.getInt("availability");
+				int hotel = res.getInt("hotel");
+				rooms.add(new RoomDTO(id, room_number, price, discounted_price, room_type, availability, hotel));
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return rooms;
 	}
-	
+
 	//return all unused rooms, plan to use this for allocating
 	//rooms to bookings
 	public List<RoomDTO> getAllAvailableRooms() {
 		return null;
 	}
-	
+
 	//return all customers, unsure if we need lists of customers
 	public List<CustomerDTO> getAllCustomers() {
 		return null;
 	}
-	
+
 	//return a single customer based on param
 	public CustomerDTO getCustomer(String param) {
 		return null;
 	}
-	
+
 	//return all staff in hotl
 	public List<StaffDTO> getAllStaff() {
 		return null;
 	}
-	
-	
+
+
 
 }
--- Assignment 2/src/edu/unsw/comp9321/jdbc/RoomDTO.java
+++ Assignment 2/src/edu/unsw/comp9321/jdbc/RoomDTO.java
@@ -1,5 +1,88 @@
 package edu.unsw.comp9321.jdbc;
 
 public class RoomDTO {
+	
+	private int id;
+	private int room_number;
+	private float price;
+	private float discounted_price;
+	private int room_type;
+	private int availability;
+	private int hotel;
+	
+	public RoomDTO (int id, int room_number, float price, float discounted_price, int room_type, int availability, int hotel) {
+		setId(id);
+		setRoom_number(room_number);
+		setPrice(price);
+		setDiscounted_price(discounted_price);
+		setRoom_type(room_type);
+		setAvailability(availability);
+		setHotel(hotel);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getRoom_number() {
+		return room_number;
+	}
+
+	public void setRoom_number(int room_number) {
+		this.room_number = room_number;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public float getDiscounted_price() {
+		return discounted_price;
+	}
+
+	public void setDiscounted_price(float discounted_price) {
+		this.discounted_price = discounted_price;
+	}
+
+	public int getRoom_type() {
+		return room_type;
+	}
+
+	public void setRoom_type(int room_type) {
+		this.room_type = room_type;
+	}
+
+	public int getAvailability() {
+		return availability;
+	}
+
+	public void setAvailability(int availability) {
+		this.availability = availability;
+	}
+
+	public int getHotel() {
+		return hotel;
+	}
+
+	public void setHotel(int hotel) {
+		this.hotel = hotel;
+	}
+	
+	
+	/*id int not null generated always as identity,
+	room_number smallint,
+	price float(10),
+	discounted_price float(20),
+	room_type smallint,
+	availability smallint,
+	hotel int,*/
 
 }
