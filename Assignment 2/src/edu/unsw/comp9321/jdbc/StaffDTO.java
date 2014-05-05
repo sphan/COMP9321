7afc8965a57360211a044bea29a164e99dfd3c35
sphan <santhai.phan@gmail.com>
2014-05-05 22:28:50 +1000

    Update StaffDTO to include staff type.

--- Assignment 2/src/edu/unsw/comp9321/jdbc/StaffDTO.java
+++ Assignment 2/src/edu/unsw/comp9321/jdbc/StaffDTO.java
@@ -5,6 +5,7 @@ public class StaffDTO {
 	private String name;
 	private String userName;
 	private String password;
+	private StaffType type;
 	
 	public StaffDTO (int id, String name, String userName, String password) {
 		this.id = id;
@@ -28,6 +29,10 @@ public class StaffDTO {
 	public String getUserName() {
 		return userName;
 	}
+	
+	public StaffType getType() {
+		return type;
+	}
 
 	public void setUserName(String userName) {
 		this.userName = userName;
@@ -41,4 +46,7 @@ public class StaffDTO {
 		this.password = password;
 	}
 
+	public void setType(String staff_type) {
+		this.type = StaffType.valueOf(staff_type.toUpperCase().replaceAll("\\W", ""));
+	}
 }
---
+++ Assignment 2/src/edu/unsw/comp9321/jdbc/StaffType.java
@@ -0,0 +1,5 @@
+package edu.unsw.comp9321.jdbc;
+
+public enum StaffType {
+	MANAGER, OWNER;
+}
