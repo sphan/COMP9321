4313d137d35f4ddfbc6f136e861bbe8b0e8fc726
mrJayson <mrJaayson@gmail.com>
2014-05-11 09:37:03 +1000

    added functions to properly handle current day incrementing

--- Assignment 2/src/edu/unsw/comp9321/jdbc/DAO.java
+++ Assignment 2/src/edu/unsw/comp9321/jdbc/DAO.java
@@ -1085,32 +1085,6 @@ public class DAO {
 		return new BookingDTO(cb_id, new CustomerDTO(c_id, c_Firstname, c_LastName), startDate, endDate, getRoomScheduleByCustomerBookingID(cb_id), getHotelByID(h_id));
 	}
 
-	private boolean isValidDate(int day, int month, int year) {
-		if (month==4||month==6||month==9||month==11) {
-			if (day<1||day>30) {
-				return false;
-			} else {
-				return true;
-			}
-		} else if (month==1||month==3||month==5||month==7||month==8||month==10||month==12) {
-			return true;
-		} else if (month==2) {
-			if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-				if (day<1||day>29) {
-					return false;
-				} else {
-					return true;
-				}
-			} else {
-				if (day<1||day>28) {
-					return false;
-				} else {
-					return true;
-				}
-			}
-		} else {
-			return false;
-		}
-	}
+
 
 }
